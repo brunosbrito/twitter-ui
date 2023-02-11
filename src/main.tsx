4e1d8235e0829cc086c40e723f4d4e1137efe49c
brunosbrito <brunobrito728@gmail.com>
2023-02-11 17:01:52 -0300

    [feat] criando onavbar do twwiter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Tweet from './components/Tweet';
+import './global.css';
+import twitterLogo from './assets/Logo.svg';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <div className='layout'>
+      <aside className='sidebar'>
+        <img className='logo' src={ twitterLogo } alt='logo' />
+        <nav className='main-navigation'>
+          <a href="">Home</a>
+          <a href="">Explore</a>
+          <a href="">Notifications</a>
+          <a href="">Messages</a>
+          <a href="">Bookmarks</a>
+          <a href="">List</a>
+          <a href="">profile</a>
+          <a href="">more</a>
+        </nav>
+        <button className='new-tweet'>Tweet</button>
+      </aside>
+      <div className="content">content</div>
+    </div>
   </React.StrictMode>,
 )
