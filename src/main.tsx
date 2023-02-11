4e44a7cee1bfb62b4205c1e49a843d5dc86e7f8a
brunosbrito <brunobrito728@gmail.com>
2023-02-11 17:38:49 -0300

    [feat] adciona icones no a de sidebar

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Tweet from './components/Tweet';
 import './global.css';
+
+import { Bell, BookBookmark, DotsThreeCircle, Envelope, FileText, Hash, House, User } from 'phosphor-react'
+
 import twitterLogo from './assets/Logo.svg';
 
 
@@ -11,14 +14,38 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <aside className='sidebar'>
         <img className='logo' src={ twitterLogo } alt='logo' />
         <nav className='main-navigation'>
-          <a href="">Home</a>
-          <a href="">Explore</a>
-          <a href="">Notifications</a>
-          <a href="">Messages</a>
-          <a href="">Bookmarks</a>
-          <a href="">List</a>
-          <a href="">profile</a>
-          <a href="">more</a>
+          <a className='active' href="">
+            <House weight='fill'/>
+            Home
+          </a>
+          <a href="">
+            <Hash />
+            Explore
+          </a>
+          <a href="">
+            <Bell /> 
+            Notifications
+          </a>
+          <a href="">
+            <Envelope />
+            Messages
+          </a>
+          <a href="">
+            <BookBookmark />
+            Bookmarks
+          </a>
+          <a href="">
+            <FileText />
+            List
+          </a>
+          <a href="">
+            <User />
+            profile
+          </a>
+          <a href="">
+            <DotsThreeCircle />
+            more
+          </a>
         </nav>
         <button className='new-tweet'>Tweet</button>
       </aside>
