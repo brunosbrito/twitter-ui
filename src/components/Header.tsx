0b5b37f4199bc8a6dad4c7493906be5f302559d3
brunosbrito <brunobrito728@gmail.com>
2023-02-13 09:41:03 -0300

    [feat] Cria o componente Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Sparkle } from "phosphor-react";
+import './HeaderStyle.css'
+
+export function Header() {
+  return(
+    <div className='content-header'>
+          <p>Home</p>
+          <Sparkle />
+        </div>
+  )
+}
